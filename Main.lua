c7bf9fcfa74e0f9f1da14807a035c95c5bde2410
0Y0X <154022703+0y0x@users.noreply.github.com>
2025-09-29 12:08:58 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/0y0x/GUI_LIB/refs/heads/main/Gui_lib.lua",true))()
+wait(0.5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0y0x/GUI_LIB/refs/heads/main/Ui.lua",true))()
